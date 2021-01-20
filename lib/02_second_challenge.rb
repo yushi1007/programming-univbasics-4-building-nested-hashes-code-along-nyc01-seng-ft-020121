de84762069e9ce4de351f7bbfa1a519c419dd326
Yu Shi <yushiys95@gmail.com>
2021-01-20 01:27:05 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -8,7 +8,10 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
   
