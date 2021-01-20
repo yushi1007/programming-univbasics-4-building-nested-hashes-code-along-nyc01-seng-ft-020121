dfa474a233a63270ca360d16ad1e9d50dbe72afd
Yu Shi <yushiys95@gmail.com>
2021-01-20 01:26:39 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+         :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
